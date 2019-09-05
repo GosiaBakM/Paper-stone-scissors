403d56afce89d2024cb6719cb1d3549de941092c
malgorzata.bak <malgorzata.bak@aspiresys.com>
2019-09-05 13:38:59 +0200

    Reorganazing order of methods. Refactoring  names of variables and methods

--- scriptGame.js
+++ scriptGame.js
@@ -1,87 +1,98 @@
-const optionImages = document.querySelectorAll('div.images img');
-const tableOfChooseOptions = ["paper", "stone", "scissors"];
-
-const printedResult = document.querySelector('.finalText');
+const imagesOfAllOptions = document.querySelectorAll('div.images img');
+const tableOfImagesNames = ["paper", "stone", "scissors"];
 
 const singularGame = {
     aISelection: "",
-    userSelection: "",
-    userWin: false
+    playerSelection: "",
+    playerVictory: false
 }
-
 const gameSummary = {
     numberOfGames: 0,
     playerResult: 0,
     AIResult: 0
 }
 
-let drawImageByAI = () => {
+const annoucmentOfResult = document.querySelector('.annoucmentOfResult');
+
+const drawImageByAI = () => {
     if (singularGame.aISelection == "") {
-        const randomIndex = Math.floor(Math.random() * tableOfChooseOptions.length);
-        singularGame.aISelection = tableOfChooseOptions[randomIndex];
-        const AIChosenImage = document.querySelector('.imageAI img');
-        AIChosenImage.dataset.status = "show";
-        AIChosenImage.src = "images/" + singularGame.aISelection + ".png";
+        const randomImagesTableIndex = Math.floor(Math.random() * tableOfImagesNames.length);
+        singularGame.aISelection = tableOfImagesNames[randomImagesTableIndex];
+        const AIChoosenImage = document.querySelector('.imageAI img');
+        AIChoosenImage.dataset.status = "show";
+        AIChoosenImage.src = "images/" + singularGame.aISelection + ".png";
+        //???
         return singularGame.aISelection;
     }
 }
 
-let checkingTheWinner = function () {
-    gameSummary.numberOfGames++;
-    if (singularGame.aISelection == singularGame.userSelection) {
-        printedResult.textContent = "It's a tie";
-    } else if ((singularGame.userSelection === "scissors" && singularGame.aISelection == "paper") || (singularGame.userSelection === "paper" && singularGame.aISelection == "stone") || (singularGame.userSelection === "stone" && singularGame.aISelection == "scissors")) {
-        printedResult.textContent = "Congrats, you won !!!";
-        gameSummary.playerResult++;
-    } else {
-        printedResult.textContent = "Sorry, you lost. Try  one more time";
-        gameSummary.AIResult++;
-        return;
-    }
-}
-
-const countingPoints = function () {
-    document.querySelector('.playerResult').textContent = 'Twoja liczba punktów: ' + gameSummary.playerResult;
-    document.querySelector('.AIResult').textContent = 'Liczba punktów przeciwnika: ' + gameSummary.AIResult;
-    document.querySelector('.gamesQuantity').textContent = 'Gry: ' + gameSummary.numberOfGames;
-}
-
-
-const selectOption = function () {
-    optionImages.forEach(function (j) {
+const changeStatusOfSelectedItem = function () {
+    imagesOfAllOptions.forEach(function (j) {
         j.setAttribute("data-selection", "notPickedOut")
     });
     this.setAttribute("data-selection", "pickedOut");
-    singularGame.userSelection = this.dataset.option;
+    singularGame.playerSelection = this.dataset.option;
+    showAfterPlayerClicking();
     drawImageByAI();
-    checkingTheWinner();
-    countingPoints();
+}
+
+let getUserChoice = function () {
+    imagesOfAllOptions.forEach(function (i) {
+        // i.style.boxShadow = "none";
+        i.addEventListener('click', changeStatusOfSelectedItem);
+    });
+}
+
+let checkTheWinner = function () {
+    gameSummary.numberOfGames++;
+    if (singularGame.aISelection == singularGame.playerSelection) {
+        annoucmentOfResult.textContent = "It's a tie";
+    } else if ((singularGame.playerSelection === "scissors" && singularGame.aISelection == "paper") || (singularGame.playerSelection === "paper" && singularGame.aISelection == "stone") || (singularGame.playerSelection === "stone" && singularGame.aISelection == "scissors")) {
+        annoucmentOfResult.textContent = "Congrats, you won !!!";
+        gameSummary.playerResult++;
+    } else {
+        annoucmentOfResult.textContent = "Sorry, you lost. Try  one more time";
+        gameSummary.AIResult++;
+    }
+}
+
+//ok
+const updateDiplayedResults = function () {
+    document.querySelector('.playerResult').textContent = 'Player score: ' + gameSummary.playerResult;
+    document.querySelector('.AIResult').textContent = 'IA score: ' + gameSummary.AIResult;
+    document.querySelector('.gamesQuantity').textContent = 'Number of games: ' + gameSummary.numberOfGames;
+}
+
+const playAgain = function () {
+    imagesOfAllOptions.forEach(function (j) {
+        j.setAttribute("data-selection", "beginPostion"); //notPickedOut
+        annoucmentOfResult.textContent = "";
+    });
+    singularGame.aISelection = "";
+    document.querySelector(".imageAI img").dataset.status = "notShow";
+    playAgainButton.dataset.status = "notShow";
+};
+
+//ok
+const displayPlayAgainButton = function () {
     const playAgainButton = document.querySelector('button.play');
     playAgainButton.dataset.status = "show";
     playAgainButton.addEventListener('click', playAgain);
 }
 
-let pickOutOption = function () {
-    optionImages.forEach(function (i) {
-        i.style.boxShadow = "none";
-        i.addEventListener('click', selectOption)
-    });
+// let showResultOfPlayers = function () {
+//     document.querySelector(".imagesNotPicked").className = "imagesPicked";
+// }
+
+const showAfterPlayerClicking = function () {
+    checkTheWinner();
+    updateDiplayedResults();
+    displayPlayAgainButton();
 }
 
-let showResultOfPlayers = function () {
-    document.querySelector(".imagesNotPicked").className = "imagesPicked";
+const gameStart = function () {
+    getUserChoice();
+
 }
 
-const startGame = function () {
-    countingPoints();
-    pickOutOption();
-}
-const playAgain = function () {
-    optionImages.forEach(function (j) {
-        j.setAttribute("data-selection", "beginPostion");
-        printedResult.textContent = "";
-    });
-    singularGame.aISelection = "";
-    document.querySelector(".imageAI img").dataset.status = "notShow";
-};
-pickOutOption();
+gameStart();
